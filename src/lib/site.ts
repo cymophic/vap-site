4014e64fa5947bbd0a385ee8cf2723ebdc8056a5
Luis Abhram <luizabhram@gmail.com>
2026-05-12 17:39:39 +0800

    feat: Add `site.ts` file with navigation links and initial site constants

---
+++ src/lib/site.ts
@@ -0,0 +1,12 @@
+// Site details
+export const websiteURL = "https://jennyannvalenciano.com";
+export const timezone = "Asia/Manila";
+export const country = "Philippines";
+
+// Navigation links for the website
+export const navLinks: { href: string; label: string }[] = [
+  { label: "Home", href: "/" },
+  { label: "About", href: "/about/" },
+  { label: "Contact", href: "/contact/" },
+  { label: "Services", href: "/services/" },
+];
