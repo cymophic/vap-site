012604b768f62d8fbe6e967f854070aa888a01cf
Luis Abhram <luizabhram@gmail.com>
2026-06-07 02:00:40 +0800

    feat: Reduce text sizes in `coming-soon` page on small screens

--- src/app/coming-soon.tsx
+++ src/app/coming-soon.tsx
@@ -14,17 +14,17 @@ export default function ComingSoon({
   return (
     <div className="flex flex-col items-center justify-center flex-1 px-7 text-center gap-8">
       <div className="flex flex-col gap-4">
-        <p className="text-5xl font-semibold tracking-tight text-brand-accent">
+        <p className="text-4xl md:text-5xl font-semibold tracking-tight text-brand-accent">
           Coming Soon
         </p>
-        <p className="text-zinc-500 text-base max-w-sm">
+        <p className="text-zinc-500 text-sm md:text-base max-w-sm">
           Something new is on the way.
           <br />
           Please check back soon.
         </p>
       </div>
 
-      <p className="text-zinc-500 max-w-sm">
+      <p className="text-zinc-500 text-sm md:text-base max-w-sm">
         In the meantime, feel free to reach out at{" "}
         <Link
           href="mailto:contact@jennyannvalenciano.com"
