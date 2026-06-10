58ab6d038db0c569de6531ec9a0278326dfecf33
Luis Abhram <luizabhram@gmail.com>
2026-06-10 16:24:29 +0800

    feat: Add Services section with 9 service cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/sections/home/Hero";
 import Clients from "@/components/sections/home/Clients";
+import Services from "@/components/sections/home/Services";
 
 export default function Home() {
   return (
     <div className="mx-auto max-w-7xl w-full flex flex-1 flex-col font-sans pt-28 px-4 xl:px-0 gap-10">
       <Hero />
       <Clients />
+      <Services />
     </div>
   );
 }
