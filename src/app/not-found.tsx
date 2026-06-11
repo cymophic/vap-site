ef0893c2c5ca1b0aa24f14c5ddcbf8972776cfab
Luis Abhram <luizabhram@gmail.com>
2026-06-11 18:29:38 +0800

    style: Refactor `not-found` component styles and center vertically

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
+import Button from "@/components/ui/Button";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center flex-1 px-7 text-center gap-6">
-      <p className="text-6xl md:text-7xl font-semibold tracking-tight text-brand-accent">
+    <div className="flex flex-col items-center justify-center flex-1 px-7 text-center gap-6  min-h-[calc(100vh-var(--header-height))]">
+      <p className="text-6xl md:text-7xl font-semibold tracking-tight text-zinc-700">
         404
       </p>
       <div className="flex flex-col gap-2">
-        <h1 className="text-xl md:text-2xl font-semibold text-brand-accent">
+        <h1 className="text-xl md:text-2xl font-semibold text-zinc-700">
           Page not found
         </h1>
         <p className="text-zinc-500 text-sm md:text-base max-w-xs">
@@ -17,9 +18,15 @@ export default function NotFound() {
       </div>
       <Link
         href="/"
-        className="text-sm md:text-base font-medium text-brand-accent underline underline-offset-4 hover:opacity-70 transition-opacity"
+        className="mt-2 w-full md:w-fit flex justify-center sm:justify-left"
       >
-        Back to home
+        <Button
+          variant="primary"
+          size="lg"
+          className="w-full md:w-auto max-w-md px-8"
+        >
+          Back to Home
+        </Button>
       </Link>
     </div>
   );
