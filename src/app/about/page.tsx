35da0f46c7a067189ec33cc0fb645fb4800c0fc6
Luis Abhram <luizabhram@gmail.com>
2026-06-16 18:05:31 +0800

    feat: Add initial About section

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/sections/about/Hero";
+import Bio from "@/components/sections/about/Bio";
+import ActionBanner from "@/components/sections/home/ActionBanner";
+
+export default function About() {
+  return (
+    <div className="mx-auto max-w-7xl w-full flex flex-1 flex-col font-sans pt-38 pb-18 px-4 xl:px-8 gap-34">
+      <Hero />
+      <Bio />
+      <ActionBanner />
+    </div>
+  );
+}
